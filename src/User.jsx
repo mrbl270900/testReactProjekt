65c9779fed60e9a81a25cf434629afac2e10c7fb
Mads <mrbl@ruc.dk>
2022-11-23 10:39:14 +0100

    new user and user file

---
+++ src/User.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const User = (props) =>
+    <div>
+        <h1>
+            {props.who.name.first}
+            {props.who.name.last}
+        </h1>
+    </div>;
+export default User;
