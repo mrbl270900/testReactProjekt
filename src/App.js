27a2c0ecd3fc617d65aaa2fc01a6a39b9a5d1eff
Mads <mrbl@ruc.dk>
2022-11-23 10:52:34 +0100

    user working with json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import User from "./User.jsx"
+import users from "./users.json"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <User who={users[0]}/>
     </div>
   );
 }
--- src/User.jsx
+++ src/User.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const User = (props) =>
     <div>
         <h1>
-            {props.who.name.first}
+            {props.who.name.first} 
             {props.who.name.last}
         </h1>
     </div>;
